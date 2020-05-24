8257660edc0b02d91eb352fc5f3d828c356c980c
amine <amine.amirouche96@hotmail.com>
2020-05-24 20:00:44 +0200

    ajou de la classe log print

---
+++ src/main/java/uvsq/pglp_3_5/LogPrint.java
@@ -0,0 +1,10 @@
+package uvsq.pglp_3_5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public  class LogPrint{
+  public static void affiche(Logger l,String msg) {
+    l.info(msg);
+  }
+}
