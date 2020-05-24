13f17ed3c32a3e6ea8267a9c48c9c0f6ee219d66
amine <amine.amirouche96@hotmail.com>
2020-05-24 20:04:07 +0200

    appel de la fonction metier dans le main

--- src/main/java/uvsq/pglp_3_5/App.java
+++ src/main/java/uvsq/pglp_3_5/App.java
@@ -6,8 +6,8 @@ package uvsq.pglp_3_5;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	  public static void main(String[] args) {
+		    ClassMetier metier = new ClassMetier();
+		    metier.methodeMetier();
+		  }
 }
